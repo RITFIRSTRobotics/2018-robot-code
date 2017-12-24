5ab5814daf4b600d576edc226aa206d0858eae36
Connor Henley <thatging3rkid@users.noreply.github.com>
2017-12-23 19:18:27 -0500

    added picon library and started refining code

--- src/robot.py
+++ src/robot.py
@@ -1,7 +1,11 @@
 import sys
 import socket
+import logging
+from logging.handlers import RotatingFileHandler
 
-from core.network.Packet import Packet
+import jsonpickle as jsonpickle
+
+from core.network.Packet import Packet, StorageType
 from core.network.constants import *
 from core.network.packetdata import MovementData
 
@@ -19,34 +23,39 @@ def process(pack):
 
 
 def main():
-    # initialize i2c and picon zero
+    # start the logger
+    logger = logging.getLogger(__name__)
+    handler = RotatingFileHandler('robot_log.log', "a", maxBytes=960000, backupCount=5)
+    logger.addHandler(handler)
 
+    # initialize i2c and picon zero
+    """"""
+
+    # Open the socket
+    logger.info("opening socket")
+    sock = socket.socket()
+
+    # Figure out and log the ip of the robot
+    ip = socket.gethostbyname(socket.gethostname())
+    logger.info("using ip: `" + ip + "`")
+    sock.bind((ip, PORT)) # bind it to the socket
+    sock.connect((FMS_IP, PORT))
+
+    # Initialization should be done now, start accepting packets
     while True:
         try:
-            # Open the socket
-            sock = socket.socket()
-
-            # Connect to the FMS
-            sock.connect((FMS_IP, PORT))
-
-            # Send a request packet
-            packet = Packet(Packet.StorageType.REQUEST, REQUEST_STR)
-            size = sock.send(packet)
-
-            # See if an error has occurred
-            if size == 0:
-                print("main(): error sending packer", file=sys.stderr)
-                continue
-
             # Get a response
             data_pack = sock.recv(BUFFER_SIZE)
+            print(data_pack.decode())
             process(data_pack)
 
-        except():
-            # do something
+        except Exception as e:
+            logger.error(e, exc_info=True)
             pass
 
     pass
 
 
-main()
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)  # logging level
+    main()
