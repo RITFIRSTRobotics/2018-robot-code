473de9e910642d9fdb84b06bd92aecb8552008c1
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2019-03-23 14:45:52 -0400

    Refactors robot control code to use NetworkManager

--- src/robot.py
+++ src/robot.py
@@ -178,15 +178,9 @@ def main():
     # initalize i2c and piconzero
     piconzero.init()
 
-    # Open the socket
-    logger.info("opening socket")
-    sock = socket.socket()
-
-    # Figure out and log the ip of the robot
-    ip = get_ip("wlan0")
-    logger.info("using ip: `" + ip + "`")
-    sock.bind((ip, PORT))  # bind it to the socket
-    sock.listen(5)  # listen for incoming data
+    # Open the socket and start the listener thread
+    netwk_mgr = NetworkManager(logger)
+    netwk_mgr.start()
 
     # Make robot stuff
     robot_disabled = True
@@ -201,76 +195,67 @@ def main():
     # Initialization should be done now, start accepting packets
     while True:
         try:
-            # Get a connection
-            csock, addr = sock.accept()
-            try:
-                pack = jsonpickle.decode(csock.recv(BUFFER_SIZE).decode())  # recieve packets, decode them, then de-json them
-            except JSONDecodeError as e:
-                print(e)
-                logger.warning(str(e))
-                continue
-            watchdog.reset()
+            raw_pack = netwk_mgr.get_next_packet()
+            if raw_pack is not None:
+                try:
+                    pack = jsonpickle.decode(raw_pack)  # recieve packets, decode them, then de-json them
+                except JSONDecodeError as e:
+                    print(e)
+                    logger.warning(str(e))
+                    continue
+                watchdog.reset()
 
-            # Type-check the data
-            if type(pack) is not Packet:
-                print("pack is not a Packet", file=sys.stderr)
-                continue
+                # Type-check the data
+                if type(pack) is not Packet:
+                    print("pack is not a Packet", file=sys.stderr)
+                    continue
 
-            # Process the packet
-            if pack.type == PacketType.STATUS:
-                # Check the contents of the packet
-                if type(pack.data) is RobotStateData:
-                    if pack.data == RobotStateData.ENABLE:
-                        robot_disabled = False
+                # Process the packet
+                if pack.type == PacketType.STATUS:
+                    # Check the contents of the packet
+                    if type(pack.data) is RobotStateData:
+                        if pack.data == RobotStateData.ENABLE:
+                            robot_disabled = False
 
-                        # Reinitialize the picon zero
-                        piconzero.init()
-                        if SHOOTER_ROBOT:
-                            piconzero.set_output_config(SHOOTER_MOTOR_CHANNEL, 1)  # set channel 0 to PWM mode
-                        if GRIPPER_ROBOT:
-                            piconzero.set_output_config(LIFT_SERVO_CHANNEL, 2)
-                            piconzero.set_output_config(GRIP_SERVO_CHANNEL, 2)  # set channel 0 and 1 to Servo mode
+                            # Reinitialize the picon zero
+                            piconzero.init()
+                            if SHOOTER_ROBOT:
+                                piconzero.set_output_config(SHOOTER_MOTOR_CHANNEL, 1)  # set channel 0 to PWM mode
+                            if GRIPPER_ROBOT:
+                                piconzero.set_output_config(LIFT_SERVO_CHANNEL, 2)
+                                piconzero.set_output_config(GRIP_SERVO_CHANNEL, 2)  # set channel 0 and 1 to Servo mode
 
-                        continue
-                    elif pack.data == RobotStateData.DISABLE:
-                        robot_disabled = True
-                        piconzero.cleanup()
-                        continue
-                    elif pack.data == RobotStateData.E_STOP:
-                        piconzero.cleanup()
-                        break
-            elif pack.type == PacketType.REQUEST:
-                # Check for the request type
-                if pack.data == RequestData.STATUS:
-                    # Send a response
-                    fms_sock = socket.socket()  # make a new socket
-                    fms_sock.settimeout(.05)
-
-                    try:
-                        fms_sock.connect((FMS_IP, PORT))
+                            continue
+                        elif pack.data == RobotStateData.DISABLE:
+                            robot_disabled = True
+                            piconzero.cleanup()
+                            continue
+                        elif pack.data == RobotStateData.E_STOP:
+                            piconzero.cleanup()
+                            break
+                elif pack.type == PacketType.REQUEST:
+                    # Check for the request type
+                    if pack.data == RequestData.STATUS:
+                        # Send a response
                         packet = Packet(PacketType.RESPONSE,  # generate a packet saying if the robot is enabled or disabled
                                         RobotStateData.DISABLE if robot_disabled else RobotStateData.ENABLE)
 
-                        fms_sock.send(jsonpickle.encode(packet).encode())  # encode and send
+                        netwk_mgr.send_packet(jsonpickle.encode(packet))
 
-                    except:
-                        pass
-
-                    fms_sock.close()
-            elif pack.type == PacketType.RESPONSE:
-                # do more stuff
-                continue
-            elif pack.type == PacketType.DATA:
-                # See if the robot is disabled
-                if robot_disabled:
+                elif pack.type == PacketType.RESPONSE:
+                    # do more stuff
                     continue
+                elif pack.type == PacketType.DATA:
+                    # See if the robot is disabled
+                    if robot_disabled:
+                        continue
 
-                # Check and see if a list of packets was sent
-                if type(pack.data) is list:
-                    for item in pack.data:
-                        process_data(item)
-                else:
-                    process_data(pack)
+                    # Check and see if a list of packets was sent
+                    if type(pack.data) is list:
+                        for item in pack.data:
+                            process_data(item)
+                    else:
+                        process_data(pack)
 
         except Exception as e:
             logger.error(e, exc_info=True)
@@ -282,20 +267,17 @@ def main():
         piconzero.cleanup()
 
         # Accept a packet
-        csock, addr = sock.accept()
-        pack = jsonpickle.decode(csock.recv(BUFFER_SIZE).decode())  # receive packets, decode them, then de-json them
+        raw_pack = netwk_mgr.get_next_packet()
+        if raw_pack is not None:
+            pack = jsonpickle.decode(raw_pack)  # receive packets, decode them, then de-json them
 
-        # Check for a request
-        if pack.type == PacketType.REQUEST:
-            # Send a response, no matter the request type
-            fms_sock = socket.socket()  # make a new socket
-            fms_sock.settimeout(.2)
-            fms_sock.connect((FMS_IP, PORT))
-            packet = Packet(PacketType.RESPONSE, RobotStateData.E_STOP)  # generate a packet saying that this robot is e-stopped
-            fms_sock.send(jsonpickle.encode(packet).encode())  # encode and send
-            fms_sock.close()
+            # Check for a request
+            if pack.type == PacketType.REQUEST:
+                # Send a response, no matter the request type
+                packet = Packet(PacketType.RESPONSE, RobotStateData.E_STOP)  # generate a packet saying that this robot is e-stopped
+                netwk_mgr.send_packet(packet)
 
-        time.sleep(.250)  # delay for 250ms, don't want to spam the picon zero with cleanup requests
+            time.sleep(.250)  # delay for 250ms, don't want to spam the picon zero with cleanup requests
     pass
 
 
